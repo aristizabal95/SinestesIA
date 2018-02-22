f8f1f14ee02de1abc9fbdb5f49c173c3e97a83c0
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2018-02-22 02:33:54 -0500

    Modified client for continuous receives.

--- client-side/websocket_client.py
+++ client-side/websocket_client.py
@@ -1,9 +1,15 @@
 from websocket import create_connection
+import signal
+import sys
+
+die = False
+def signal_handler(signal, frame):
+    global die
+    print('Killing connection')
+    die = True
+
 ws = create_connection("ws://192.168.0.101:8080")
-print("Sending 'Hello World'...")
-ws.send("Hello world")
-print("Sent")
-print("Receving")
-result = ws.recv()
-print("Received: '%s'" % result)
+while(not die):
+    result = ws.recv()
+    print("Received: '%s'" % result)
 ws.close()
