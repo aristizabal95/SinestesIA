1668ad941aef81b592732aecd5a8391910ccffde
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2018-02-23 01:24:44 -0500

    make client receive continuous stream.

--- client-side/websocket_client.py
+++ client-side/websocket_client.py
@@ -2,14 +2,13 @@ from websocket import create_connection
 import signal
 import sys
 
-die = False
 def signal_handler(signal, frame):
     global die
     print('Killing connection')
     die = True
 
 ws = create_connection("ws://192.168.0.101:8080")
-while(not die):
+while(True):
     result = ws.recv()
     print("Received: '%s'" % result)
 ws.close()
