e477d257b5097c0d726e44af6db4d6f980fef8a9
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2018-02-24 17:54:50 -0500

    Make client create image with received data

--- client-side/websocket_client.py
+++ client-side/websocket_client.py
@@ -1,6 +1,8 @@
 from websocket import create_connection
 import signal
 import sys
+import numpy as np
+from PIL import Image
 
 def signal_handler(signal, frame):
     global die
@@ -10,5 +12,10 @@ def signal_handler(signal, frame):
 ws = create_connection("ws://192.168.0.101:8080")
 while(True):
     result = ws.recv()
-    print("Received: '%s'" % result)
+    # print("Received: '%s'" % result)
+    data = np.frombuffer(result, dtype=np.uint8)
+    data = np.reshape(data, (640, 480, 3))
+    im = Image.fromarray(data)
+    im.save('rgb.jpg')
+    print(data.shape)
 ws.close()
