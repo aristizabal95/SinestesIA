b46320982a1576efb7668864aaa5c8b8f5e70c26
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2018-03-22 01:04:16 -0500

    create servo thread, merge with command handler

--- src/command_handler.h
+++ src/command_handler.h
@@ -1,7 +1,9 @@
 #ifndef COMMAND_HANDLER_H
 #define COMMAND_HANDLER_H
 
+void stop(int signum);
 void error(char *msg);
+void *servo_threadfunc(void *arg);
 void *tcp_threadfunc(void *arg);
 
 #endif
