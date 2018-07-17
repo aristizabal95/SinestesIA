42b75a504cfd971b046c56026a1020912480f5f0
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2018-07-16 23:30:56 -0500

    clear unnecessary code, transmit depth data

--- src/ffmpeg_stream.c
+++ src/ffmpeg_stream.c
@@ -12,13 +12,14 @@
 void *ffmpeg_threadfunc(void *arg) {
 	FILE *pipeout; // This will be the pipe that will write to ffmpeg
 
-	pipeout = popen("ffmpeg -threads 6 -r 25 -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 640x480 -i - -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params 00108310518720928b30d38f41149351559761969b71d79f8218a39259a7 -preset ultrafast -vcodec libx264 -tune zerolatency -crf 40 -b 5000k -f rtp_mpegts srtp://192.168.0.100:1234", "w");
+	pipeout = popen("ffmpeg -threads 6 -r 25 -f rawvideo -vcodec rawvideo -pix_fmt rgb24 -s 1280x480 -i - -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params 00108310518720928b30d38f41149351559761969b71d79f8218a39259a7 -preset ultrafast -vcodec libx264 -tune zerolatency -crf 40 -b 5000k -f rtp_mpegts srtp://192.168.0.100:1234", "w");
 	while(!die) {
 		pthread_mutex_lock(&streaming_mutex);
-		if(messageReady) {
-			fwrite(messageBuffer, 1, 640*480*3, pipeout);
+		if(rgbReady && depthReady) {
+			fwrite(messageBuffer, 1, 640*480*3*2, pipeout);
 			fflush(pipeout);
-			messageReady = 0;
+			rgbReady = 0;
+			depthReady = 0;
 		}
 		pthread_mutex_unlock(&streaming_mutex);
 	}
