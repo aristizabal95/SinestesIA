4f7bd214c3256c289b3e08ff15fd260c1a458dce
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2018-08-24 09:37:50 -0500

    Make adjustments for local work

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <pigpio.h>
+// #include <pigpio.h>
 #include <string.h>
 #include "libfreenect.h"
 // #include <libfreenect/libfreenect.h>
